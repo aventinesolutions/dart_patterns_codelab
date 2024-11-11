4f7b764745d63ad50054c3da858f6b8c18a7aec8
Matthew Evan Eichler <matthew.evaneichler@luminis.eu>
2024-11-11 16:23:45 +0100

    :bear: :construction: add a global function to use a switch for format Durations to Strings

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,19 @@ void main() {
   runApp(const DocumentApp());
 }
 
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
+}
+
 class DocumentApp extends StatelessWidget {
   const DocumentApp({super.key});
 
