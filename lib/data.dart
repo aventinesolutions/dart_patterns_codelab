1ef409ba721e380c827574f86519247f4b78bec2
Matthew Evan Eichler <matthew.evaneichler@luminis.eu>
2024-11-12 11:11:21 +0100

    :bear: :construction: fine tuning of the Human Readable version of the Modified Data

--- lib/data.dart
+++ lib/data.dart
@@ -62,7 +62,7 @@ const documentJson = '''
 {
   "metadata": {
     "title": "Aventine Solutions Document Repository",
-    "modified": "2024-10-31"
+    "modified": "2024-12-25"
   },
   "blocks": [
     {
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,10 @@ String formatDate(DateTime dateTime) {
     Duration(inDays: 0) => 'today',
     Duration(inDays: 1) => 'tomorrow',
     Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 && days ~/ 7 == 1 => 'one week from today',
     Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days) when days < -7 && days.abs() ~/ 7 == 1 =>
+      'one week ago',
     Duration(inDays: final days) when days < -7 =>
       '${days.abs() ~/ 7} weeks ago',
     Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
